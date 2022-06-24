d58ab54cff19166202eb1c8e58e75c531ed6b444
Anotherfella <45348494+Anotherfella@users.noreply.github.com>
2022-06-25 04:16:06 +0530

    Added solution - LeetHub

---
+++ Optimal Strategy For A Game - GFG/optimal-strategy-for-a-game.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+//Function to find the maximum possible amount of money we can win.
+class Solution{
+    public:
+    long long   dp[1001][1001];
+      long long opt( int arr[], int i, int j)
+    { if(i>j)return 0;
+      if (dp[i][j]!=-1) return dp[i][j];
+           int  a = arr[i]+min(opt(arr,i+2,j),opt(arr,i+1,j-1));
+           int b = arr[j] +min(opt(arr,i,j-2), opt(arr,i+1,j-1));
+           
+           dp[i][j] = 1ll*max(a,b);
+            return dp[i][j]; 
+       
+        
+    }
+    
+    long long maximumAmount(int arr[], int n){
+          dp[n][n];
+           memset(dp,-1,sizeof(dp));
+            return opt(arr,0,n-1);
+        
+        // Your code here
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{
+   
+   	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+		cin>>a[i];
+		Solution ob;
+		cout<< ob.maximumAmount(a,n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
