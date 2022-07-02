d01d7374739f9901fd86318fdd65b0c32114576f
Anotherfella <45348494+Anotherfella@users.noreply.github.com>
2022-07-03 00:25:45 +0530

    Time: 11 ms (62.76%), Space: 8 MB (91.37%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -3,16 +3,16 @@ public:
      int dp[51][51];
     
      int kill( int  l, int r, vector<int>&ans)
-     { if(l>=r) return 0;
+     { if(r-l<2) return 0;
        int res =  INT_MAX;
        
         if(dp[l][r]!=-1)
         {
              return dp[l][r];
         }
-       for( int i = l; i<r; i++)
+       for( int i = l+1; i<r; i++)
        {
-            res  =  min(res,kill(l,i,ans)+kill(i+1,r,ans)+(ans[l-1]*ans[i]*ans[r]));
+            res  =  min(res,kill(l,i,ans)+kill(i,r,ans)+(ans[l]*ans[i]*ans[r]));
            
        }
        return dp[l][r] = res;
@@ -22,7 +22,7 @@ public:
     int minScoreTriangulation(vector<int>& ans) {
          memset(dp,-1,sizeof(dp));
           int n =ans.size();
-         return kill(1,n-1,ans);
+         return kill(0,n-1,ans);
         
     }
 };
