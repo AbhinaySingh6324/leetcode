3a52ffdafa67018adb33715c3a68f5498b659d26
Anotherfella <45348494+Anotherfella@users.noreply.github.com>
2022-07-11 23:43:49 +0530

    Time: 665 ms (12.93%), Space: 117 MB (65.99%) - LeetHub

---
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -0,0 +1,33 @@
+class MedianFinder {
+public:
+    
+    priority_queue<int> small;
+    priority_queue<int,vector<int>,greater<int>> large;
+    bool even;
+    MedianFinder() {
+        even = true;
+    }
+    
+    void addNum(int num) {
+        if(even)
+        {
+            large.push(num);
+            small.push(large.top());
+            large.pop();
+        }
+        else
+        {
+            small.push(num);
+            large.push(small.top());
+            small.pop();
+        }
+        even=!even;
+    }
+    
+    double findMedian() {
+        if(even)
+            return (small.top()+large.top())/2.0;
+        else
+            return small.top();
+    }
+};
